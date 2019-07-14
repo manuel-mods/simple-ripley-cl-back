9c4c8cbcb129ca306837a84780658b713de4db02
Manuel Bastias <mibastias@falabella.cl>
2019-07-14 19:41:05 -0400

    cambio rutas

--- app/routes/products.route.js
+++ app/routes/products.route.js
@@ -1,5 +1,5 @@
 
 const { ProductsController } = require('../controllers/products.controller');
 module.exports = (app) => {
-  app.get('/products/:sku' , ProductsController.get);
+  app.get('api/v1/product/:sku' , ProductsController.get);
 };
